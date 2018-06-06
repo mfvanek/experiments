cb6e7a76a93c804c2301d4de005bfb0560fcab84
Ivan Vakhrushev <ivan.vakhrushev@akvelon.com>
2018-06-06 15:12:16 +0300

    WinJS simple application

---
+++ WinJSAppSample/WinJSApp1/js/family.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    "use strict";
+    WinJS.Namespace.define("MyFamily", {
+        members: new WinJS.Binding.List([
+            { id: 1, label: "Me", name: "Ivan" },
+            { id: 2, label: "Wife", name: "Alexandra" },
+            { id: 3, label: "Son", name: "Saveliy" },
+            { id: 4, label: "Daughter", name: "Taisiya" }
+        ])
+    });
+})();
