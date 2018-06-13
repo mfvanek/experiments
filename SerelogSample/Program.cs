d88399b5543bb481794bb3e6be7323da4784c839
Ivan Vakhrushev <ivan.vakhrushev@akvelon.com>
2018-06-13 10:31:10 +0300

    Serilog example with ForContext<>()

---
+++ SerelogSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using Serilog;
+using System;
+
+namespace SerelogSample
+{
+    class Program
+    {
+        private const int COUNT = 1000;
+        private const int THRESHOLD = COUNT + 1; // decrease this value to get log file in app folder
+        private static readonly ILogger Logger;
+
+        static Program()
+        {
+            const string template = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [{SourceContext:l}] {Message:lj} <{ThreadId}>{NewLine}{Exception}";
+            var config = new LoggerConfiguration();
+            config.MinimumLevel.Debug()
+                .Enrich.WithThreadId()
+                .WriteTo.Console(outputTemplate: template)
+                .WriteTo.File("mySerilogTest.log", rollingInterval: RollingInterval.Day, outputTemplate: template);
+            Log.Logger = config.CreateLogger();
+            Logger = Log.Logger.ForContext<Program>();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Serilog");
+            for (uint i = 1; i <= COUNT; ++i)
+            {
+                Logger.Information($"counter = {i}");
+            }
+            new SimpleDuck().Quack();
+            Console.WriteLine("Test is done. Press any key and then check app folder");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SerelogSample/SimpleDuck.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace SerelogSample
+{
+    class SimpleDuck
+    {
+        private static readonly ILogger Logger = Log.Logger.ForContext<SimpleDuck>();
+
+        public void Quack()
+        {
+            Logger.Information("Duck says: \"Quack-quack-quack\"");
+        }
+    }
+}
