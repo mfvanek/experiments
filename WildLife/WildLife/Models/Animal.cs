b7658227459da2547193578f7ef469696e9af8e4
Ivan Vakhrushev <ivan.vakhrushev@akvelon.com>
2018-11-27 12:42:57 +0300

    Wildlife sample

---
+++ WildLife/WildLife/Models/Animal.cs
@@ -0,0 +1,20 @@
+﻿namespace WildLife.Models
+{
+    public abstract class Animal
+    {
+
+        public bool IsCarnivorous { get; protected set; }
+
+        public bool IsHerbivorous { get => !IsCarnivorous; }
+
+        public abstract string Cry();
+
+        public abstract void Jump();
+
+        public abstract void Run();
+
+        public abstract void Bite(Animal target);
+
+        public abstract bool Attack(Animal target);
+    }
+}
---
+++ WildLife/WildLife/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WildLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
