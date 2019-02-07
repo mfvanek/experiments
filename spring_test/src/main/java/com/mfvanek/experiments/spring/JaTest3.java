ac1faab661ed7174eebb1e94f70982e42287c215
Ivan Vakhrushev <mfvanek@gmail.com>
2019-02-07 23:48:35 +0300

    my final variant

--- spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest3.java
+++ spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest3.java
@@ -4,26 +4,33 @@ import java.io.*;
 
 public class JaTest3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             int n = Integer.parseInt(reader.readLine());
+            // TODO dynamic array is needed here
+            int[] array = new int[n];
             int current = 0;
+            int idx = 0;
+            int item;
             boolean first = true;
-            int item = 0;
             for (int i = 0; i < n; ++i) {
                 item = Integer.parseInt(reader.readLine());
                 if (first) {
                     current = item;
-                    System.out.println(item);
+                    array[idx] = item;
+                    ++idx;
                     first = false;
                 } else {
-                    if (item != current) {
+                    if (current != item) {
                         current = item;
-                        System.out.println(item);
+                        array[idx] = item;
+                        ++idx;
                     }
                 }
             }
-        } catch (IOException e) {
+            for (int i = 0; i < idx; ++i) {
+                System.out.println(array[i]);
+            }
         }
     }
 }
