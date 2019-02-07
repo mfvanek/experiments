71d8d6eebab76e7c02a1992f925e6d75ba02d62e
Ivan Vakhrushev <mfvanek@gmail.com>
2019-02-07 23:23:31 +0300

    next variant

--- spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest3.java
+++ spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest3.java
@@ -5,32 +5,32 @@ import java.io.*;
 public class JaTest3 {
 
     public static void main(String[] args) {
-
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             int n = Integer.parseInt(reader.readLine(), 10);
-            int[] arr = new int[n];
+            //int[] arr = new int[n];
             int current = 0;
-            int idx = 0;
+            //int idx = 0;
             boolean first = true;
             for (int i = 0; i < n; ++i) {
                 int item = Integer.parseInt(reader.readLine(), 10);
                 if (first) {
                     current = item;
-                    arr[idx] = item;
-                    ++idx;
+                    //arr[idx] = item;
+                    System.out.println(item);
+                    //++idx;
                     first = false;
                 } else {
                     if (item != current) {
                         current = item;
-                        arr[idx] = item;
-                        ++idx;
+                        //arr[idx] = item;
+                        System.out.println(item);
+                        //++idx;
                     }
                 }
             }
-
-            for (int i = 0; i < idx; ++i) {
-                System.out.println(arr[i]);
-            }
+//            for (int i = 0; i < idx; ++i) {
+//                System.out.println(arr[i]);
+//            }
         } catch (IOException e) {
         }
     }
