0768f7081990c70668cea93aba4d4e1bcd1dfdb9
Ivan Vakhrushev <mfvanek@gmail.com>
2019-02-10 20:45:54 +0300

    Via files...

--- spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest3.java
+++ spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest3.java
@@ -1,41 +1,29 @@
 package com.mfvanek.experiments.spring;
 
 import java.io.*;
-import java.util.*;
 
 public class JaTest3 {
 
     public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("D:\\src\\experiments\\spring_test\\src\\main\\resources\\input.txt"));
+             PrintWriter writer = new PrintWriter(new FileWriter("output.txt"))) {
             int n = Integer.parseInt(reader.readLine());
-            int delta = Math.max(n / 10, 1);
-            int curSize = delta;
-            int[] array = new int[curSize];
             int current = 0;
-            int idx = 0;
             int item;
             boolean first = true;
             for (int i = 0; i < n; ++i) {
                 item = Integer.parseInt(reader.readLine());
                 if (first) {
                     current = item;
-                    array[idx] = item;
-                    ++idx;
+                    writer.println(item);
                     first = false;
                 } else {
                     if (current != item) {
                         current = item;
-                        if (curSize == idx) {
-                            curSize = curSize + delta;
-                            array = Arrays.copyOf(array, curSize);
-                        }
-                        array[idx] = item;
-                        ++idx;
+                        writer.println(item);
                     }
                 }
-            }
-            for (int i = 0; i < idx; ++i) {
-                System.out.println(array[i]);
+                // writer.flush();
             }
         }
     }
