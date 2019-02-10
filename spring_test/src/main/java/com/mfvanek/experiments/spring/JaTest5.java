5a00283756425d1b1558b852d5d47c295e86fe3d
Ivan Vakhrushev <mfvanek@gmail.com>
2019-02-10 22:20:00 +0300

    Pass

--- spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest5.java
+++ spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest5.java
@@ -8,7 +8,6 @@ public class JaTest5 {
         try (Scanner scanner = new Scanner(System.in)) {
             String first = scanner.nextLine();
             String second = scanner.nextLine();
-            //System.out.println(check(first, second));
             char[] a1 = first.toCharArray();
             char[] a2 = second.toCharArray();
             Arrays.sort(a1);
@@ -16,14 +15,4 @@ public class JaTest5 {
             System.out.println(Arrays.equals(a1, a2) ? 1 : 0);
         }
     }
-
-//    private static int check(String first, String second) {
-//        if (first.length() != second.length()) {
-//            return 0;
-//        } else if (first.equals(second)) {
-//            return 1;
-//        }
-//        long ma = first.chars().distinct().filter(x -> second.chars().anyMatch(y -> y == x)).count();
-//        return distincts == 0L ? 1 : 0;
-//    }
 }
