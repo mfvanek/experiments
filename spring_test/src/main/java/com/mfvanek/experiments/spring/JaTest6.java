3072b448e64277cd22c95f144d3f01b2e91d6213
Ivan Vakhrushev <mfvanek@gmail.com>
2019-02-11 21:42:55 +0300

    Not pass

--- spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest6.java
+++ spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest6.java
@@ -2,6 +2,7 @@ package com.mfvanek.experiments.spring;
 
 import java.util.*;
 
+// Try to use https://en.wikipedia.org/wiki/External_sorting
 public class JaTest6 {
 
     public static void main(String[] args) {
@@ -9,12 +10,12 @@ public class JaTest6 {
             int k = Integer.parseInt(scanner.nextLine(), 10);
             if (k > 0) {
                 int[][] arrays = new int[k][];
-                //int total = 0;
+                int total = 0;
                 for (int i = 0; i < k; ++i) {
                     String[] tmp = scanner.nextLine().split(" ");
                     if (tmp.length > 0) {
                         int n = Integer.parseInt(tmp[0], 10);
-                        //total += n;
+                        total += n;
                         arrays[i] = new int[n];
                         for (int j = 1; j < tmp.length && j < n + 1; ++j) {
                             arrays[i][j - 1] = Integer.parseInt(tmp[j], 10);
@@ -22,38 +23,19 @@ public class JaTest6 {
                     }
                 }
 
-                int[] result = arrays[0];
-                for (int i = 1; i < k; ++i) {
-                    result = merge(result, arrays[i]);
+                int r = 0;
+                int[] result = new int[total];
+                for (int i = 0; i < k; ++i) {
+                    for (int j = 0; j < arrays[i].length; ++j) {
+                        result[r++] = arrays[i][j];
+                    }
                 }
 
+                Arrays.sort(result);
                 for (int i : result) {
                     System.out.print(i + " ");
                 }
             }
         }
-    }
-
-    private static int[] merge(int[] a, int[] b) {
-        int[] res = new int[a.length + b.length];
-
-        int i = 0, j = 0, r = 0;
-        while (i < a.length && j < b.length) {
-            if (a[i] < b[j]) {
-                res[r++] = a[i++];
-            } else {
-                res[r++] = b[j++];
-            }
-        }
-
-        while (i < a.length) {
-            res[r++] = a[i++];
-        }
-
-        while (j < b.length) {
-            res[r++] = b[j++];
-        }
-
-        return res;
     }
 }
