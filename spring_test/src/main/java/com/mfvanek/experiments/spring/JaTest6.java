985ca4876c984853c4490bb818aaeed95e963d08
Ivan Vakhrushev <mfvanek@gmail.com>
2019-02-12 22:34:16 +0300

    next variant

--- spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest6.java
+++ spring_test/src/main/java/com/mfvanek/experiments/spring/JaTest6.java
@@ -1,46 +1,59 @@
 package com.mfvanek.experiments.spring;
 
 import java.io.*;
+import java.nio.file.*;
 import java.util.*;
 
-// Try to use https://en.wikipedia.org/wiki/External_sorting
 public class JaTest6 {
 
     private static int counter = 0;
 
-    public static void main(String[] args) throws Exception {
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+    public static void main(String[] args) {
+        try (BufferedReader input = new BufferedReader(new FileReader("input.txt"));
+             PrintWriter output = new PrintWriter(new FileWriter("output.txt"))) {
             counter = 0;
-            int k = Integer.parseInt(reader.readLine(), 10);
+            int k = Integer.parseInt(input.readLine(), 10);
             if (k > 0) {
-                String[] files = new String[k];
+                Deque<String> files = new LinkedList<>();
                 for (int i = 0; i < k; ++i) {
-                    String[] tmp = reader.readLine().split(" ");
+                    String[] tmp = input.readLine().split(" ");
                     if (tmp.length > 1) {
                         int n = Integer.parseInt(tmp[0], 10);
                         if (n > 0) {
                             final String fileName = makeFileName();
-                            files[i] = fileName;
+                            files.addLast(fileName);
                             try (PrintWriter writer = new PrintWriter(new FileWriter(fileName))) {
-                                for (String value : tmp) {
-                                    writer.println(value);
+                                for (int j = 0; j < tmp.length && j < n + 1; ++j) {
+                                    writer.println(tmp[j]);
                                 }
                             }
                         }
                     }
                 }
 
-//                for (String i : files) {
-//                    System.out.println(i);
-//                }
+                while (files.size() > 1) {
+                    final String newFile = merge(files.removeFirst(), files.removeFirst());
+                    files.addLast(newFile);
+                }
 
-                System.out.println(merge(files[0], files[1]));
+                toOutput(output, files.peekFirst());
             }
+        } catch (Exception e) {
         }
     }
 
     private static String makeFileName() {
         return String.format("output_%d.txt", counter++);
+    }
+
+    private static void toOutput(PrintWriter output, String fileName) throws IOException {
+        try (BufferedReader input = new BufferedReader(new FileReader(fileName))) {
+            int n = Integer.parseInt(input.readLine(), 10);
+            for (int i = 0; i < n; ++i) {
+                output.print(input.readLine() + " ");
+            }
+        }
+        Files.delete(Paths.get(fileName));
     }
 
     private static String merge(String firstFile, String secondFile) throws IOException {
@@ -88,6 +101,71 @@ public class JaTest6 {
                 }
             }
         }
+
+        Files.delete(Paths.get(firstFile));
+        Files.delete(Paths.get(secondFile));
         return fileName;
     }
 }
+
+/*
+package com.mfvanek.experiments.spring;
+
+import java.util.*;
+
+public class JaTest6 {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int k = Integer.parseInt(scanner.nextLine(), 10);
+            if (k > 0) {
+                int[][] arrays = new int[k][];
+                //int total = 0;
+                for (int i = 0; i < k; ++i) {
+                    String[] tmp = scanner.nextLine().split(" ");
+                    if (tmp.length > 0) {
+                        int n = Integer.parseInt(tmp[0], 10);
+                        //total += n;
+                        arrays[i] = new int[n];
+                        for (int j = 1; j < tmp.length && j < n + 1; ++j) {
+                            arrays[i][j - 1] = Integer.parseInt(tmp[j], 10);
+                        }
+                    }
+                }
+
+                int[] result = arrays[0];
+                for (int i = 1; i < k; ++i) {
+                    result = merge(result, arrays[i]);
+                }
+
+                for (int i : result) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
+    private static int[] merge(int[] a, int[] b) {
+        int[] res = new int[a.length + b.length];
+
+        int i = 0, j = 0, r = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                res[r++] = a[i++];
+            } else {
+                res[r++] = b[j++];
+            }
+        }
+
+        while (i < a.length) {
+            res[r++] = a[i++];
+        }
+
+        while (j < b.length) {
+            res[r++] = b[j++];
+        }
+
+        return res;
+    }
+}
+*/
