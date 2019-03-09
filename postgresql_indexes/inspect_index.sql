4eae02c98e74a20ca15b23d9e8c7f7e204872595
Ivan Vakhrushev <mfvanek@gmail.com>
2019-03-09 10:46:37 +0300

    Added index stat script

---
+++ postgresql_indexes/inspect_index.sql
@@ -0,0 +1,26 @@
+select * from pg_stat_all_indexes where schemaname <> 'pg_catalog' and indexrelname like 'idx_ref%';
+select * from pg_indexes where schemaname <> 'pg_catalog' and indexname like 'idx_ref%';
+
+select
+  t.tablename,
+  indexname,
+  c.reltuples::bigint AS num_rows,
+  pg_size_pretty(pg_relation_size(quote_ident(t.tablename)::text)) AS table_size,
+  pg_size_pretty(pg_relation_size(quote_ident(indexrelname)::text)) AS index_size,
+  CASE WHEN indisunique THEN 'YES' ELSE 'NO' END AS UNIQUE,
+  idx_scan AS number_of_scans,
+  idx_tup_read AS tuples_read,
+  idx_tup_fetch AS tuples_fetched
+FROM pg_tables t
+       LEFT OUTER JOIN pg_class c ON t.tablename=c.relname
+       LEFT OUTER JOIN
+     ( SELECT c.relname AS ctablename, ipg.relname AS indexname, x.indnatts AS number_of_columns, idx_scan, idx_tup_read, idx_tup_fetch, indexrelname, indisunique FROM pg_index x
+                                                                                                                                                                          JOIN pg_class c ON c.oid = x.indrelid
+                                                                                                                                                                          JOIN pg_class ipg ON ipg.oid = x.indexrelid
+                                                                                                                                                                          JOIN pg_stat_all_indexes psai ON x.indexrelid = psai.indexrelid AND psai.schemaname = 'public'
+       )
+       AS foo
+     ON t.tablename = foo.ctablename
+WHERE t.schemaname='public'
+and indexname like 'idx_ref%'
+ORDER BY 1,2;
